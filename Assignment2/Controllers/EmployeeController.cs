fca28beea7f961140281497d75f9f5fc8f609ee7
Dan Brennan <DanBrennan33@hotmail.com>
2016-04-29 08:28:15 -0400

    Added files via upload

---
+++ Assignment2/Controllers/EmployeeController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment2.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private Manager man = new Manager();
+
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View(man.EmployeeGetAll());
+        }
+
+        // GET: Employee/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+                return HttpNotFound();
+            else
+                return View(man.EmployeeGetOne(id));
+        }
+
+        // GET: Employee/Create
+        public ActionResult Create()
+        {
+            return View(new EmployeeBase());
+        }
+
+        // POST: Employee/Create
+        [HttpPost]
+        public ActionResult Create(EmployeeAdd NewEmployee)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                if (!ModelState.IsValid)
+                {
+                    return View(NewEmployee);
+                }
+                var AddedEmployee = man.EmployeeAdd(NewEmployee);
+                if (AddedEmployee == null)
+                {
+                    return View(NewEmployee);
+                }
+                else
+                {
+                    return RedirectToAction("details", new { id = AddedEmployee.EmployeeId });
+                }
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employee/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Employee/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employee/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Employee/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Assignment2/Controllers/Manager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+// new...
+using AutoMapper;
+using Assignment2.Models;
+
+namespace Assignment2.Controllers
+{
+    public class Manager
+    {
+        // Reference to the data context
+        private DataContext ds = new DataContext();
+
+        public Manager()
+        {
+            // If necessary, add constructor code here
+        }
+
+        // Add methods below
+        // Controllers will call these methods
+        // Ensure that the methods accept and deliver ONLY view model objects and collections
+        // The collection return type is almost always IEnumerable<T>
+
+        // Suggested naming convention: Entity + task/action
+        // For example:
+        // ProductGetAll()
+        // ProductGetById()
+        // ProductAdd()
+        // ProductEdit()
+        // ProductDelete()
+
+        public IEnumerable<EmployeeBase> EmployeeGetAll()
+        {
+            var c = ds.Employees.OrderBy(e => e.LastName).ThenBy(e => e.FirstName);
+
+            return Mapper.Map<IEnumerable<EmployeeBase>>(c);
+        }
+        public EmployeeBase EmployeeGetOne(int? id)
+        {
+            var d = ds.Employees.Find(id);
+
+            return (d == null) ? null : Mapper.Map<EmployeeBase>(d);
+        }
+        public EmployeeBase EmployeeAdd(EmployeeAdd newEmployee)
+        {
+            var AddedEmployee = ds.Employees.Add(Mapper.Map<Employee>(newEmployee));
+            ds.SaveChanges();
+
+            return (AddedEmployee == null) ? null : Mapper.Map<EmployeeBase>(newEmployee);
+        }
+    }
+}
